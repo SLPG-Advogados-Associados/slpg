37ccee220bdf4c79cb42f1f25d92da127814dde1
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-15 20:32:19 +0100

    chore: added initial static file typings

---
+++ frontend/@types/static/index.d.ts
@@ -0,0 +1,15 @@
+declare module "*.svg" {
+  const url: string;
+
+  export default url;
+}
+
+declare module "*.graphql";
+
+declare module "*.gql";
+
+declare module "*.png" {
+  const url: string;
+
+  export default url;
+}
