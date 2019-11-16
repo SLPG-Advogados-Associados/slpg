d846a1b5baf48006d7d51af25d401ad3d266cb96
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-16 22:36:25 +0100

    chore: configured eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,38 @@
+const extensions = [
+  // JavaScript
+  '.js',
+  '.json',
+  '.mjs',
+  '.es',
+  '.node',
+  '.jsx',
+  // TypeScript
+  '.ts',
+  '.d.ts',
+  '.tsx',
+]
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      node: { extensions },
+      alias: {
+        map: [
+          ['~app', `${__dirname}/src`],
+          ['~design', `${__dirname}/src/design`],
+        ],
+        extensions,
+      },
+    },
+  },
+}
