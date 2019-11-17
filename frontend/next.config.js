aa8ead483a868cd7fa2b16a2818cc30395e9f13f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-17 19:02:25 +0100

    chore: avoid TypeScript failure from blocking next build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,18 @@
-const compose = require('next-compose')
+const compose = require('next-compose-plugins')
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
-module.exports = compose([[withImages], [withCSS]])
+const plugins = [[withImages], [withCSS]]
+
+const config = {
+  webpack: config => {
+    // disable TypeScript checks (use `yarn type-check` and editor plugins instead)
+    config.plugins = config.plugins.filter(
+      plugin => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin'
+    )
+
+    return config
+  },
+}
+
+module.exports = compose(plugins, config)
