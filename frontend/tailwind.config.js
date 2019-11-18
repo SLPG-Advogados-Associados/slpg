58204a24035d3ca514706b255035f547a9d4286d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-18 23:14:09 +0100

    feat: added multiple theme variables

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,12 +6,43 @@ const t = path => theme => R.path(path.split('.'), theme)
 const initial = {
   colors: {
     black: '#010101',
+    white: '#ffffff',
 
     gray: {
       default: '#7F7F7F',
+      300: '#E8E6E3',
       500: '#bcbcbd',
       700: '#6b6c6d',
     },
+
+    darkBlue: '#072242',
+  },
+
+  fontSize: {
+    100: '0.75rem', //  12px
+    200: '0.875rem', // 14px
+    400: '1rem', //     16px <-- base
+    500: '1.125rem', // 18px
+    600: '1.25rem',
+    700: '1.5rem', //   24px
+    800: '1.75rem', //  28px
+    1000: '2rem', //    32px
+    1200: '3rem',
+    1400: '4rem',
+  },
+
+  spacing: {
+    xs: '0.5rem',
+    sm: '0.75rem',
+    md: '1rem',
+    lg: '2rem',
+    xl: '4rem',
+  },
+}
+
+const semantic = {
+  colors: {
+    primary: t('colors.darkBlue'),
   },
 }
 
@@ -19,10 +50,37 @@ const functional = {
   colors: {
     body: t('colors.gray.500'),
     heading: t('colors.gray.700'),
+    link: t('colors.darkBlue'),
+    button: t('colors.white'),
+    divisor: t('colors.gray.300'),
+  },
+
+  backgroundColor: {
+    button: t('colors.darkBlue'),
+  },
+
+  fontSize: {
+    footnote: t('fontSize.100'),
+    // 200
+    meta: t('fontSize.200'),
+    button: t('fontSize.200'),
+    // 400
+    base: t('fontSize.400'),
+    menu: t('fontSize.400'),
+    text: t('fontSize.400'),
+    // 500
+    aside: t('fontSize.500'),
+    // 700
+    cta: t('fontSize.700'),
+    'item-title': t('fontSize.700'),
+    // 800
+    title: t('fontSize.800'),
+    // 1000
+    heading: t('fontSize.1000'),
   },
 }
 
-const extend = [initial, functional].reduce((theme, curr) => {
+const extend = [initial, semantic, functional].reduce((theme, curr) => {
   for (const key in curr) {
     let val = curr[key]
 
@@ -56,6 +114,8 @@ const extend = [initial, functional].reduce((theme, curr) => {
 
   return theme
 }, defaultTheme)
+
+console.log({ extend })
 
 module.exports = {
   theme: { extend },
