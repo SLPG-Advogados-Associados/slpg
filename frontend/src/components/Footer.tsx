a580568af25badd4ec199a7d46c218ffedfcdac3
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-27 13:15:06 +0100

    feat: added Footer navigation section

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,93 @@
 import React from 'react'
-import { Container } from '~design'
+import styled from 'styled-components'
+import { theme } from 'styled-tools'
+import { Container, Button, AsideTitle, Logo, Icons, classed } from '~design'
+import { list as expertises } from '~modules/expertise'
+
+const FooterColumn = styled(classed.div`px-md`)`
+  flex-grow: 1;
+  flex-basis: 0;
+`
+
+const FooterSocial = styled.ul`
+  margin: ${theme('spacing.4')} 0;
+
+  li {
+    display: inline-block;
+    margin: ${theme('spacing.2')};
+  }
+`
+
+const FooterMenu = styled.ul`
+  li {
+    margin-top: ${theme('spacing.2')};
+  }
+`
+
+const FooterLogo = styled(Logo)`
+  max-width: 320px;
+`
 
 const Footer = () => (
   <footer>
+    <Container className="flex py-12 text-200">
+      <FooterColumn className="text-center">
+        <a href="/" title="Início" className="inline-block">
+          <FooterLogo />
+        </a>
+        <p>OAB/SC 270/97</p>
+        <Button small>Receba nossos informativos</Button>
+        <FooterSocial>
+          <li>
+            <Button circle small>
+              <Icons.Facebook />
+            </Button>
+          </li>
+          <li>
+            <Button circle small>
+              <Icons.YouTube />
+            </Button>
+          </li>
+        </FooterSocial>
+      </FooterColumn>
+
+      <FooterColumn>
+        <AsideTitle>Áreas de atuação</AsideTitle>
+
+        <FooterMenu>
+          {expertises.map(({ id, label, href }) => (
+            <li key={id}>
+              <a href={href} title={label}>
+                {label}
+              </a>
+            </li>
+          ))}
+        </FooterMenu>
+      </FooterColumn>
+
+      <FooterColumn>
+        <AsideTitle>Menu</AsideTitle>
+
+        <FooterMenu>
+          <li>
+            <a href="/blogue" title="Blogue">
+              Blogue
+            </a>
+          </li>
+          <li>
+            <a href="/contato" title="Contato">
+              Contato
+            </a>
+          </li>
+          <li>
+            <a href="/quem-somos" title="Quem somos">
+              Quem somos
+            </a>
+          </li>
+        </FooterMenu>
+      </FooterColumn>
+    </Container>
+
     <div className="bg-reverse text-white py-3 text-center">
       <Container>
         Florianópolis/SC. Rua: Nunes Machado, ed. Tiradentes, nº 94, 9º andar.
