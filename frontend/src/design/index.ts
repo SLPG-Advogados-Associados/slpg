abedb7a7ee1ecc0a901d6a18acdc60e6b7f3d886
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-27 13:13:33 +0100

    feat: added Icons

--- frontend/src/design/index.ts
+++ frontend/src/design/index.ts
@@ -3,6 +3,7 @@ export * from './lib/classed'
 
 // components
 export * from './components/Logo'
+export * from './components/Icons'
 export * from './components/Title'
 export * from './components/Button'
 export * from './components/Container'
