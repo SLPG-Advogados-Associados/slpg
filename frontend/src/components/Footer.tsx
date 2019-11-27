cb75325e5ad5298540daabbe837823c8e5e4ab38
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-27 12:27:54 +0100

    feat: fixed footer styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Container } from '~design'
 
 const Footer = () => (
   <footer>
-    <div className="bg-reverse text-white py-3 mt-5 text-center">
+    <div className="bg-reverse text-white py-3 text-center">
       <Container>
         Florianópolis/SC. Rua: Nunes Machado, ed. Tiradentes, nº 94, 9º andar.
         CEP 88010-460. Telefone: (48) 3024-4166
