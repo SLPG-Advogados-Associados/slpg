8cb1ddb1177512c938beefc7d7d4dbbf00c89dd5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-27 01:13:37 +0100

    feat: added home-page heading

--- frontend/src/pages/index.page.tsx
+++ frontend/src/pages/index.page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
+import { theme } from 'styled-tools'
 import { Page } from '~app/components/Page'
 import { withGraphQL, GT } from '~api'
 import { Container, Heading } from '~design'
@@ -6,11 +8,28 @@ import { PostListItem } from '~modules/blog'
 import { useQuery } from '@apollo/react-hooks'
 import { BLOG_LATEST } from './index.gql'
 
+const Header = styled.div`
+  height: 450px;
+  padding: 60px;
+  font-size: ${theme('fontSize.heading')};
+  text-align: center;
+  color: white;
+  background: url('/background.jpg') center no-repeat;
+  background-size: cover;
+`
+
 const HomePage = () => {
   const blog = useQuery<GT.BLOG_LATEST_QUERY>(BLOG_LATEST)
 
   return (
     <Page>
+      <Header>
+        <p className="mb-4">
+          Nosso trabalho é defender os direitos da classe trabalhadora.
+        </p>
+        <p className="mb-4">Ligue: (48) 3024-4166</p>
+      </Header>
+
       <Container>
         <Heading>Blogue</Heading>
 
