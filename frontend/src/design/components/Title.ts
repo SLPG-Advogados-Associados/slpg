21069e6b8276504311846e2731ba59aa3804f5e3
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-27 13:14:21 +0100

    feat: added AsideTitlte

--- frontend/src/design/components/Title.ts
+++ frontend/src/design/components/Title.ts
@@ -3,5 +3,6 @@ import * as classed from '../lib/classed-tags'
 const Heading = classed.h2`text-heading font-semibold mt-16 mb-10 text-center uppercase`
 const Title = classed.h2`text-title font-semibold my-4 text-center uppercase`
 const ItemTitle = classed.h3`text-item-title font-semibold mb-2`
+const AsideTitle = classed.h4`text-aside font-bold uppercase mb-2`
 
-export { Title, ItemTitle, Heading }
+export { Title, ItemTitle, Heading, AsideTitle }
