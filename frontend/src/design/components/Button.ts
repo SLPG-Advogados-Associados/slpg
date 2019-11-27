8500704e0a136b067689521cb18a65a54b50bf09
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-27 13:14:09 +0100

    feat: added Button variants "circle" and "small"

--- frontend/src/design/components/Button.ts
+++ frontend/src/design/components/Button.ts
@@ -1,17 +1,41 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { ifProp } from 'styled-tools'
 import * as classed from '../lib/classed-tags'
 
+interface Props {
+  alt?: string
+  href?: string
+  title?: string
+  target?: string
+  // variants
+  small?: boolean
+  circle?: boolean
+}
+
+const small = css`
+  font-size: 0.95em;
+`
+
+const circle = css`
+  display: inline-flex;
+  justify-content: center;
+  height: 3.653em;
+  width: 3.653em;
+  padding: 0;
+  border-radius: 50%;
+`
+
 const Button = styled(
-  classed.button`text-button bg-button hover:bg-button:active` as React.FC<{
-    alt?: string
-    href?: string
-    title?: string
-    target?: string
-  }>
+  classed.button`text-button bg-button hover:bg-button--active` as React.FC<
+    Props
+  >
 )`
   display: inline-block;
   padding: 1.175em 1.875em 1.05em;
+
+  ${ifProp('small', small)}
+  ${ifProp('circle', circle)}
 `
 
 export { Button }
