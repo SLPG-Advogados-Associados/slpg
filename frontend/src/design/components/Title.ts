eb5307b6e71a643eafeaa2667de3f8ff0ff0d071
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-28 20:56:01 +0100

    feat: added TextualTitle component

--- frontend/src/design/components/Title.ts
+++ frontend/src/design/components/Title.ts
@@ -16,9 +16,11 @@ const c = (component: AnyStyledComponent) =>
   `
 
 // prettier-ignore
-const Heading = c(classed.h2`text-heading font-semibold mt-16 mb-10 text-center uppercase`)
+const Heading = c(classed.h2`text-heading mt-16 mb-10 text-center uppercase`)
 const Title = c(classed.h2`text-title font-semibold my-4 text-center uppercase`)
+// prettier-ignore
+const TextualTitle = c(classed.h2`text-textual-title my-4 text-left pb-2 border-b`)
 const ItemTitle = c(classed.h3`text-item-title font-semibold mb-2`)
 const AsideTitle = c(classed.h4`text-aside font-bold uppercase mb-2`)
 
-export { Title, ItemTitle, Heading, AsideTitle }
+export { Title, TextualTitle, ItemTitle, Heading, AsideTitle }
