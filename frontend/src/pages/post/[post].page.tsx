51d18b1855b47e71db8c1cbef193cb9024d41ab6
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-28 13:46:38 +0100

    feat: added image to /post/:id page

--- frontend/src/pages/post/[post].page.tsx
+++ frontend/src/pages/post/[post].page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Page } from '~app/components/Page'
 import { withGraphQL, GT } from '~api'
-import { Container, Title } from '~design'
+import { Container, Title, Heading, styled } from '~design'
 import { useQuery } from '@apollo/react-hooks'
 import { BLOG_POST } from './post.gql'
 import { useRouter } from '~app/lib/router'
+
+const StyledHeading = styled(Heading)`
+  margin: 0;
+`
+
+const Image = styled.img`
+  display: inline-block;
+  max-height: 40vh;
+  max-width: 50vw;
+  margin: 2rem 0 3rem;
+`
 
 const PostPage = () => {
   const { query: variables } = useRouter<{ post: string }>()
@@ -14,9 +25,19 @@ const PostPage = () => {
 
   return (
     <Page>
+      <div className="bg-reverse text-white py-8">
+        <StyledHeading>Blogue</StyledHeading>
+      </div>
+
       {post ? (
-        <Container>
+        <Container className="py-12">
           <Title>{post.title}</Title>
+
+          {post.image ? (
+            <div className="text-center">
+              <Image src={'http://localhost:1337' + post.image.url} />
+            </div>
+          ) : null}
 
           <div>{post.body}</div>
         </Container>
