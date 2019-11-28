cc2f9dce4b0ce83563f4be3b6bbcb5060b172928
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-28 15:44:29 +0100

    refactor: simplified variants styled helper

--- frontend/src/design/lib/styled.ts
+++ frontend/src/design/lib/styled.ts
@@ -6,10 +6,8 @@ import * as styledTools from 'styled-tools'
 const predicate = <P extends {}>(fn, apply: Interpolation<P>, props: P) =>
   fn(props) ? apply : null
 
-const variants = <P extends {}>(
-  map: { [key: string]: Interpolation<any> },
-  props: P
-) => Object.keys(map).map(prop => styledTools.ifProp(prop, map[prop]))
+const variants = <P extends {}>(map: { [key: string]: Interpolation<any> }) =>
+  Object.keys(map).map(prop => styledTools.ifProp(prop, map[prop]))
 
 // "t" stands for "theme", but it's just a shortcut aggregator anyway.
 const t = { ...styledTools, if: curry(predicate), variants: curry(variants) }
