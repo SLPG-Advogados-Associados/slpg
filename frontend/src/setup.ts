860bb1e1692e440bff80d5c42566e4277e453ebf
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-29 15:17:06 +0100

    feat: improved SPA navigation scroll

--- frontend/src/setup.ts
+++ frontend/src/setup.ts
@@ -1,4 +1,8 @@
+import { Router } from 'next/router'
 import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
 
 dayjs.locale('pt-br')
+
+// eslint-disable-next-line no-undef
+Router.events.on('routeChangeComplete', () => window.scrollTo(0, 0))
