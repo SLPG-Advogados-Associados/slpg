26ba58b69e216370199edb0738c1c331aad7d90d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-03 18:28:51 +0100

    feat: added sample-content page actual editable content

--- frontend/src/pages/_dev/sample-content.page.tsx
+++ frontend/src/pages/_dev/sample-content.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Page } from '~app/components/Page'
 import { Section } from '~app/components/Section'
+import { react as Content } from '~content/sample.md'
 
 const SampleContentPage = () => (
   <Page>
     <Section>Hello!</Section>
+    <Content />
   </Page>
 )
 
