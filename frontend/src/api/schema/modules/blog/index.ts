af7ddfe0fdba9d53c74841283fd30ce7bc3e91c8
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-03 19:16:28 +0100

    feat: added Query::posts resolution

--- frontend/src/api/schema/modules/blog/index.ts
+++ frontend/src/api/schema/modules/blog/index.ts
@@ -1,10 +1,34 @@
 import { GT } from '~api'
+import { blog } from '~content'
 import typeDefs from './blog.graphql'
 
+const posts = blog.keys()
+
 const Query: GT.QueryResolvers = {
-  posts: () => [],
+  posts: (_root, { limit = 10, start = 0 }) => {
+    if (limit > 50) {
+      throw new Error('Cannot require more than 50 posts at once')
+    }
+
+    const items = posts.slice(start, limit)
+
+    return {
+      id: `${limit}-${start}`,
+      count: items.length,
+      total: posts.length,
+      items,
+    }
+  },
 }
 
-const resolvers = { Query }
+const PostsResult: GT.PostsResult = {
+  items: ({ items }: { items: string[] }) => items.map(id => ({ id })),
+}
+
+const PostsResultItem: GT.PostsResultItem = {
+  item: ({ id }) => ({ ...blog(id).attributes, id }),
+}
+
+const resolvers = { Query, PostsResult, PostsResultItem }
 
 export { typeDefs, resolvers }
