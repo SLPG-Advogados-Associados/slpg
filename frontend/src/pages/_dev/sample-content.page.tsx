8d2b4ba34a439bb8e8fe25060c6a79d975720b78
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-03 14:43:34 +0100

    test: added sample-content.page for testing purposes

---
+++ frontend/src/pages/_dev/sample-content.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Page } from '~app/components/Page'
+import { Section } from '~app/components/Section'
+import { attributes, react as SampleContent } from '~content/sample.md'
+
+const SampleContentPage = () => (
+  <Page>
+    <Section>Hello!</Section>
+  </Page>
+)
+
+export default SampleContentPage
