0769795b7f652216948051afc8935b21dce27880
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-07 20:01:51 +0100

    feat: added map to contact page

--- src/pages/contato.page.tsx
+++ src/pages/contato.page.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { withGraphQL } from '~api'
 import { Page } from '~app/components/Page'
-import { Heading } from '~design'
+import { Heading, styled } from '~design'
 import { Section } from '~app/components/Section'
+
+const Map = styled.iframe.attrs({
+  src:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3535.829957690526!2d-48.55102268497343!3d-27.598800982837005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952738254a4b6e49%3A0xb0290625e1d82ca9!2sSLPG%20Advogados%20Associados!5e0!3m2!1spt-BR!2scz!4v1575744925010!5m2!1spt-BR!2scz',
+  frameBorder: '0',
+  allowFullScreen: true,
+})`
+  height: 400px;
+  width: 100%;
+`
 
 const ContatoPage = () => {
   return (
@@ -37,8 +47,10 @@ const ContatoPage = () => {
             Catarina
           </p>
 
-          <p className="font-bold">Telefone/FAX: (48) 3024-4166</p>
+          <p className="font-bold mb-0">Telefone/FAX: (48) 3024-4166</p>
         </Section>
+
+        <Map />
       </main>
     </Page>
   )
