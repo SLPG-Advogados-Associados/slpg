e494a8a38df3a2476a58894c6400ec944489c65b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-07 16:53:05 +0100

    feat: added generic team loader

--- src/admin/content/index.ts
+++ src/admin/content/index.ts
@@ -1,4 +1,6 @@
 // @ts-ignore
 const blog = require.context('./blog/', true, /^(.*\.(md$))[^.]*$/imu)
+// @ts-ignore
+const team = require.context('./team/', true, /^(.*\.(json$))[^.]*$/imu)
 
-export { blog }
+export { blog, team }
