a4faa2d0d008adeb8563092bbd8033a4a1360009
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-09 01:48:06 +0100

    feat: added youtube editor component

---
+++ src/admin/cms.js
@@ -0,0 +1,22 @@
+const youTube = {
+  id: 'youtube',
+  label: 'YouTube',
+  fields: [{ name: 'url', label: 'YouTube Video URL', widget: 'string' }],
+  pattern: /^\[\[youtube:(\S+)\]\]$/,
+  fromBlock: match => ({ url: match[1] }),
+  toBlock: ({ url }) => `[[youtube:${url}]]`,
+  toPreview: ({ url }) => `
+    <div style="overflow: hidden; padding-bottom: 56.25%; position: relative; height: 0;">
+      <iframe
+        style="left: 0;top: 0;height: 100%;width: 100%; position: absolute;"
+        width="853"
+        height="505"
+        src="${url}"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>`,
+}
+
+CMS.registerEditorComponent(youTube)
