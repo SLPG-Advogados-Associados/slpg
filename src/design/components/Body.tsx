20775ec0043be0e86873178215c403a1ca1d297c
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-12-09 18:16:30 +0100

    feat: open external links in new tabs

--- src/design/components/Body.tsx
+++ src/design/components/Body.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Markdown from 'react-markdown'
 import { HTMLContent } from './HTMLContent'
+import parse from 'url-parse'
+
+// Netlify environment holds a `URL` env for the target origin.
+const origin =
+  process.env.URL || (process.browser ? window.location.origin : '')
+
+// Use `_blank` for external links.
+const linkTarget = (url: string) =>
+  parse(url).origin === origin ? '_self' : '_blank'
 
 const Body: React.FC<{ content: string }> = ({ content, ...props }) => (
   <HTMLContent {...props}>
-    <Markdown source={content} />
+    <Markdown source={content} linkTarget={linkTarget} />
   </HTMLContent>
 )
 
