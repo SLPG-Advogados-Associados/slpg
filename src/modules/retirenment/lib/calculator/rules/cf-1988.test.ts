dc2911b9198680d155fc779257939aefe56ce419
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-12 19:44:53 +0100

    test: simplified cf-1988 with custom jest matchers

--- src/modules/retirenment/lib/calculator/rules/cf-1988.test.ts
+++ src/modules/retirenment/lib/calculator/rules/cf-1988.test.ts
@@ -1,18 +1,29 @@
 import { Gender } from '../types'
 import { conditions } from './cf-1988'
+import { isEqual } from 'date-fns'
 
 const { MALE, FEMALE } = Gender
 
-describe('retirement/calculator/rules/cf-1998', () => {
-  const getInput = (gender: Gender, birth: string, start: string) => ({
-    gender,
-    birthDate: new Date(birth),
-    contribution: { start: new Date(start) },
-  })
+/**
+ * Generates a valid cf-1988 rule input.
+ */
+const getInput = (gender: Gender, birth: string, start: string) => ({
+  gender,
+  birthDate: new Date(birth),
+  contribution: { start: new Date(start) },
+})
 
+/**
+ * Factory for reached date matcher.
+ */
+const reachedAtFactory = condition => year => input =>
+  isEqual(condition(input)[1].reached.getFullYear(), year)
+
+describe('retirement/calculator/rules/cf-1998', () => {
   describe('conditions', () => {
     describe('a)', () => {
       const condition = conditions[0]
+      const reachedAt = reachedAtFactory(condition)
 
       it('should check qualification', () => {
         // qualified
@@ -24,26 +35,15 @@ describe('retirement/calculator/rules/cf-1998', () => {
       })
 
       describe('context', () => {
-        it('should return reached date in context', () => {
+        it('should return correct reached date in context', () => {
           // male, contributed since 1960, reached in 35 years
-          expect(
-            condition(getInput(MALE, '1940', '1960'))[1].reached.getFullYear()
-          ).toBe(1995)
-
+          expect(getInput(MALE, '1940', '1960')).toSatisfy(reachedAt(1995))
           // female, contributed since 1965, reached in 30 years
-          expect(
-            condition(getInput(FEMALE, '1940', '1965'))[1].reached.getFullYear()
-          ).toBe(1995)
-
+          expect(getInput(FEMALE, '1940', '1965')).toSatisfy(reachedAt(1995))
           // male, contributed since 1970, reached in 35 years
-          expect(
-            condition(getInput(MALE, '1940', '1970'))[1].reached.getFullYear()
-          ).toBe(2005)
-
+          expect(getInput(MALE, '1940', '1970')).toSatisfy(reachedAt(2005))
           // female, contributed since 1970, reached in 30 years
-          expect(
-            condition(getInput(FEMALE, '1940', '1970'))[1].reached.getFullYear()
-          ).toBe(2000)
+          expect(getInput(FEMALE, '1940', '1970')).toSatisfy(reachedAt(2000))
         })
       })
     })
