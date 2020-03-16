5c2463fdd332494707e652c0a5c23c58d305f217
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-16 17:20:58 +0100

    feat: added initial 1998 proportional rule

--- src/modules/retirenment/lib/calculator/rules/1998-ec-20-transition.ts
+++ src/modules/retirenment/lib/calculator/rules/1998-ec-20-transition.ts
@@ -20,8 +20,34 @@ export interface ConditionContext extends ConditionContextBase {
  */
 const due = new Date('2003-12-31')
 
+const reusable = {
+  /**
+   * (...)
+   * I - tiver cinqüenta e três anos de idade, se homem, e quarenta e oito anos
+   * de idade, se mulher;
+   * (...)
+   */
+  age: {
+    [MALE]: age(due)(53),
+    [FEMALE]: age(due)(48),
+  },
+
+  contribution: {
+    /**
+     * (...)
+     * II - tiver cinco anos de efetivo exercício no cargo em que se dará a
+     * aposentadoria;
+     * (...)
+     */
+    last: contribution.last(due)(5),
+  },
+}
+
 const conditions: Condition<Input, ConditionContext>[] = [
   /**
+   * Integral
+   * --------
+   *
    * I - tiver cinqüenta e três anos de idade, se homem, e quarenta e oito anos
    * de idade, se mulher;
    *
@@ -40,21 +66,8 @@ const conditions: Condition<Input, ConditionContext>[] = [
     const integrality = true
 
     const subConditions = [
-      /**
-       * (...)
-       * I - tiver cinqüenta e três anos de idade, se homem, e quarenta e oito anos
-       * de idade, se mulher;
-       * (...)
-       */
-      age(due)({ [MALE]: 53, [FEMALE]: 48 }[input.gender]),
-
-      /**
-       * (...)
-       * II - tiver cinco anos de efetivo exercício no cargo em que se dará a
-       * aposentadoria;
-       * (...)
-       */
-      contribution.last(due)(5),
+      reusable.age[input.gender],
+      reusable.contribution.last,
 
       /**
        * (...)
@@ -70,6 +83,53 @@ const conditions: Condition<Input, ConditionContext>[] = [
        * @todo: a) and b) not currently considered!
        */
       contribution.total(due)({ [MALE]: 35, [FEMALE]: 30 }[input.gender]),
+    ]
+
+    const [satisfied, { reached }] = merge.all(subConditions)(input)
+
+    return [satisfied, { reached, integrality }]
+  },
+
+  /**
+   * Proporcional
+   * ------------
+   *
+   * § 1º - O servidor de que trata este artigo, desde que atendido o disposto
+   * em seus incisos I e II, e observado o disposto no art. 4º desta Emenda,
+   * pode aposentar-se com proventos proporcionais ao tempo de contribuição,
+   * quando atendidas as seguintes condições;
+   *
+   * I - contar tempo de contribuição igual, no mínimo, à soma de:
+   *
+   * a) trinta anos, se homem, e vinte e cinco anos, se mulher; e
+   *
+   * b) um período adicional de contribuição equivalente a quarenta por cento do
+   * tempo que, na data da publicação desta Emenda, faltaria para atingir o
+   * limite de tempo constante da alínea anterior;
+   *
+   * @todo: a) and b) not currently considered!
+   */
+  input => {
+    const integrality = false
+
+    const subConditions = [
+      reusable.age[input.gender],
+      reusable.contribution.last,
+
+      /**
+       * (...)
+       * III - contar tempo de contribuição igual, no mínimo, à soma de:
+       *
+       * a) trinta e cinco anos, se homem, e trinta anos, se mulher; e
+       *
+       * b) um período adicional de contribuição equivalente a vinte por cento do
+       * tempo que, na data da publicação desta Emenda, faltaria para atingir o
+       * limite de tempo constante da alínea anterior.
+       * (...)
+       *
+       * @todo: a) and b) not currently considered!
+       */
+      contribution.total(due)({ [MALE]: 30, [FEMALE]: 25 }[input.gender]),
     ]
 
     const [satisfied, { reached }] = merge.all(subConditions)(input)
