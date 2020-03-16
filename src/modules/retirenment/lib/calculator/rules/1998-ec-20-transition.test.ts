1d95e7b50b8e6ca6325b87ab4451cb71cea18b22
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-16 13:04:23 +0100

    test: minor test readability refactor

--- src/modules/retirenment/lib/calculator/rules/1998-ec-20-transition.test.ts
+++ src/modules/retirenment/lib/calculator/rules/1998-ec-20-transition.test.ts
@@ -37,27 +37,18 @@ describe('retirement/calculator/rules/1998-ec-20-transition', () => {
      */
     describe('main', () => {
       const condition = conditions[0]
-      // const reachedAt = reachedAtFactory(condition)
-      // const hasIntegrality = input => condition(input)[1].integrality === true
 
       it.each([
-        // male, 54 ✅, contributing 36 ✅, last more than 5 ✅
-        [getInput(M, '1949', [['1967', '1977'], ['1977']]), true],
-        // male, 52 ❌, contributing 36 ✅, last more than 5 ✅
-        [getInput(M, '1951', [['1967', '1977'], ['1977']]), false],
-        // male, 54 ✅, contributing 34 ❌, last more than 5 ✅
-        [getInput(M, '1949', [['1967', '1977'], ['1979']]), false],
-        // male, 54 ✅, contributing 36 ✅, last less than 5 ❌
-        [getInput(M, '1949', [['1967', '2000'], ['2000']]), false],
-
-        // female, 49 ✅, contributing 31 ✅, last more than 5 ✅
-        [getInput(F, '1954', [['1972', '1977'], ['1977']]), true],
-        // female, 47 ❌, contributing 31 ✅, last more than 5 ✅
-        [getInput(F, '1956', [['1972', '1977'], ['1977']]), false],
-        // female, 49 ✅, contributing 29 ❌, last more than 5 ✅
-        [getInput(F, '1954', [['1972', '1977'], ['1979']]), false],
-        // female, 49 ✅, contributing 31 ✅, last less than 5 ❌
-        [getInput(F, '1954', [['1972', '2000'], ['2000']]), false],
+        // male
+        [getInput(M, '1949', [['1967', '1977'], ['1977']]), true], //   male, 54 ✅, contributing 36 ✅, last more than 5 ✅
+        [getInput(M, '1951', [['1967', '1977'], ['1977']]), false], //  male, 52 ❌, contributing 36 ✅, last more than 5 ✅
+        [getInput(M, '1949', [['1967', '1977'], ['1979']]), false], //  male, 54 ✅, contributing 34 ❌, last more than 5 ✅
+        [getInput(M, '1949', [['1967', '2000'], ['2000']]), false], //  male, 54 ✅, contributing 36 ✅, last less than 5 ❌
+        // female
+        [getInput(F, '1954', [['1972', '1977'], ['1977']]), true], //   female, 49 ✅, contributing 31 ✅, last more than 5 ✅
+        [getInput(F, '1956', [['1972', '1977'], ['1977']]), false], //  female, 47 ❌, contributing 31 ✅, last more than 5 ✅
+        [getInput(F, '1954', [['1972', '1977'], ['1979']]), false], //  female, 49 ✅, contributing 29 ❌, last more than 5 ✅
+        [getInput(F, '1954', [['1972', '2000'], ['2000']]), false], //  female, 49 ✅, contributing 31 ✅, last less than 5 ❌
       ])('should check qualification', (input, satisfied) => {
         expect(condition(input)[0]).toBe(satisfied)
       })
