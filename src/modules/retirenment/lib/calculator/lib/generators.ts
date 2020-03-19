69877ea051f68c5d9efabe5f5079db0583197f40
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-19 01:29:04 +0100

    feat: added date parsing for 2 digit year string

--- src/modules/retirenment/lib/calculator/lib/generators.ts
+++ src/modules/retirenment/lib/calculator/lib/generators.ts
@@ -1,9 +1,17 @@
-type DateParams = ConstructorParameters<typeof Date>
+export type DateParams = ConstructorParameters<typeof Date>
 
 /**
  * Syntax sugar for `new Date` constructor. Good for shorter tests.
  */
-const date = (...input: DateParams) => new Date(...input)
+const date = (...input: DateParams) => {
+  const [year] = input
+
+  if (input.length === 1 && typeof year === 'string' && year.length === 2) {
+    return new Date(`${year[0] === '0' ? '20' : '19'}${year}`)
+  }
+
+  return new Date(...input)
+}
 const d = date // alias
 
 /**
