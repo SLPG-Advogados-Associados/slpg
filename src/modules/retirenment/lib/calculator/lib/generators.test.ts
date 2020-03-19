dcbcd1601567b934ad1020ed9facbeb77fd1fe1d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-19 01:29:04 +0100

    feat: added date parsing for 2 digit year string

--- src/modules/retirenment/lib/calculator/lib/generators.test.ts
+++ src/modules/retirenment/lib/calculator/lib/generators.test.ts
@@ -1,18 +1,24 @@
-import { date, birth } from './generators'
+import { DateParams, date, birth } from './generators'
 
 describe('retirement/calculator/lib/generators', () => {
   describe('date', () => {
-    type Items = ConstructorParameters<typeof Date>[][]
+    type Items = [DateParams, number][]
 
     it.each([
-      [['2020']],
-      [[0]],
-      [['December 17, 1995 03:24:00']],
-      [['1995-12-17T03:24:00']],
-      [[1995, 11, 17]],
-      [[1995, 11, 17, 3, 24, 0]],
-    ] as Items)('should generate valid dates', input => {
-      expect(date(...input)).toEqual(new Date(...input))
+      // default
+      [['2020'], 2020],
+      [[0], 1970],
+      [['December 17, 1995 03:24:00'], 1995],
+      [['1995-12-17T03:24:00'], 1995],
+      [[1995, 11, 17], 1995],
+      [[1995, 11, 17, 3, 24, 0], 1995],
+      // extra
+      [['00'], 2000],
+      [['05'], 2005],
+      [['10'], 1910],
+      [['60'], 1960],
+    ] as Items)('should generate valid dates', (input, year) => {
+      expect(date(...input).getFullYear()).toEqual(year)
     })
   })
 
