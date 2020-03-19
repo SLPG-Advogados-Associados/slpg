42f5a310419e0b2c9eb229b9612ffe1ac0afd2a9
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-19 01:02:50 +0100

    feat: added birthDate generator

--- src/modules/retirenment/lib/calculator/lib/generators.ts
+++ src/modules/retirenment/lib/calculator/lib/generators.ts
@@ -1,6 +1,15 @@
+type DateParams = ConstructorParameters<typeof Date>
+
 /**
  * Syntax sugar for `new Date` constructor. Good for shorter tests.
  */
-const d = (...input: ConstructorParameters<typeof Date>) => new Date(...input)
+const date = (...input: DateParams) => new Date(...input)
+const d = date // alias
 
-export { d }
+/**
+ * Constructs an object containing `birthDate` prop.
+ */
+const birth = (...input: DateParams) => ({ birthDate: date(...input) })
+const b = birth // alias
+
+export { date, d, birth, b }
