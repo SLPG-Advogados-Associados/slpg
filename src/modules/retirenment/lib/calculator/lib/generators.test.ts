b7dffa032a90de38c16da1d50df10999baa4e74e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-03-19 01:02:50 +0100

    feat: added birthDate generator

--- src/modules/retirenment/lib/calculator/lib/generators.test.ts
+++ src/modules/retirenment/lib/calculator/lib/generators.test.ts
@@ -1,4 +1,4 @@
-import * as gen from './generators'
+import { date, birth } from './generators'
 
 describe('retirement/calculator/lib/generators', () => {
   describe('date', () => {
@@ -12,7 +12,22 @@ describe('retirement/calculator/lib/generators', () => {
       [[1995, 11, 17]],
       [[1995, 11, 17, 3, 24, 0]],
     ] as Items)('should generate valid dates', input => {
-      expect(gen.d(...input)).toEqual(new Date(...input))
+      expect(date(...input)).toEqual(new Date(...input))
+    })
+  })
+
+  describe('birth', () => {
+    type Items = ConstructorParameters<typeof Date>[][]
+
+    it.each([
+      [['2020']],
+      [[0]],
+      [['December 17, 1995 03:24:00']],
+      [['1995-12-17T03:24:00']],
+      [[1995, 11, 17]],
+      [[1995, 11, 17, 3, 24, 0]],
+    ] as Items)('should generate valid dates', input => {
+      expect(birth(...input)).toEqual({ birthDate: new Date(...input) })
     })
   })
 })
