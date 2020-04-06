20c31924b341a05f0fbdd3c8f9e8e252cc6b1650
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-06 23:26:13 +0200

    refactor: updated core condition signature

--- src/modules/retirenment/lib/calculator/lib/conditions/conditions.test.ts
+++ src/modules/retirenment/lib/calculator/lib/conditions/conditions.test.ts
@@ -1,7 +1,7 @@
 /* cspell: disable */
 import { isValid } from 'date-fns'
 import { Condition } from '../../types'
-import { d, b, reachedAt } from '../test-utils'
+import { eq, d, b } from '../test-utils'
 
 // @ts-ignore
 import { age, merge, contribution, __set__ } from '.'
@@ -18,21 +18,14 @@ describe('retirement/calculator/lib/conditions', () => {
 
   describe('age', () => {
     it.each([
-      [[d('2000'), 50, b('1940')], true], //   ✅ 50+ by 2000
-      [[d('2000'), 50, b('1950')], true], //   ✅ 50 by 2000
-      [[d('2000'), 50, b('1960')], false], //  ❌ 40 by 2000
-      [[d('2000'), 30, b('1960')], true], //   ✅ 40 by 2000
-      [[d('2000'), 30, b('1970')], true], //   ✅ 30 by 2000
-      [[d('2000'), 30, b('1980')], false], //  ❌ 20 by 2000
-    ] as const)('should correctly qualify', ([due, years, input], expected) => {
-      expect(age(due)({ years })(input)[0]).toBe(expected)
-    })
-
-    it.each([
-      [[d('2000'), 50, b('1940')], 1990], // reached 50 yo from 1940
-      [[d('2000'), 20, b('1940')], 1960], // reached 20 yo from 1940
-    ] as const)('should reach correctly', ([due, years, input], expected) => {
-      expect(age(due)({ years })(input)).toSatisfy(reachedAt(expected))
+      [50, '1940', 1990],
+      [50, '1950', 2000],
+      [50, '1960', 2010],
+      [30, '1960', 1990],
+      [30, '1970', 2000],
+      [30, '1980', 2010],
+    ] as const)('should correctly qualify', (years, birth, reached) => {
+      expect(age({ years })(b(birth)).reached).toSatisfy(eq.date(reached))
     })
   })
 
@@ -42,12 +35,12 @@ describe('retirement/calculator/lib/conditions', () => {
 
     const conds: CondsDictiorary = {
       pass: {
-        on1990: () => [true, { reached: d('1990') }],
-        on2000: () => [true, { reached: d('2000') }],
+        on1990: () => ({ reached: d('1990') }),
+        on2000: () => ({ reached: d('2000') }),
       },
       fail: {
-        on1995: () => [false, { reached: d('1995') }],
-        on2005: () => [false, { reached: d('2005') }],
+        on1995: () => ({ reached: d('1995') }),
+        on2005: () => ({ reached: d('2005') }),
       },
     }
 
@@ -59,8 +52,7 @@ describe('retirement/calculator/lib/conditions', () => {
       })
 
       it('should handle empty condition set', () => {
-        const [satisfied, { reached }] = all([])({})
-        expect(satisfied).toBe(true)
+        const { reached } = all([])({})
         expect(reached).toBeInstanceOf(Date)
         expect(reached).not.toSatisfy(isValid)
       })
@@ -69,15 +61,6 @@ describe('retirement/calculator/lib/conditions', () => {
         const left = all([conds.pass.on1990])({})
         const right = conds.pass.on1990({})
         expect(left).toMatchObject(right)
-      })
-
-      it.each([
-        [all([conds.pass.on1990]), true], //                      ✅ single one (pass.on1990) succeded
-        [all([conds.pass.on1990, conds.pass.on2000]), true], //   ✅ both (pass.on1990, pass.on2000) succeded
-        [all([conds.pass.on1990, conds.fail.on2005]), false], //  ❌ one (fail.on2005) fails
-        [all([conds.fail.on1995, conds.fail.on2005]), false], //  ❌ both (fail.on1995, fail.on2005) fails
-      ])('should only satisfy if all satisfy', (cond, satisfied) => {
-        expect(cond({})[0]).toBe(satisfied)
       })
 
       it.each([
@@ -90,7 +73,7 @@ describe('retirement/calculator/lib/conditions', () => {
         [all([conds.fail.on1995, conds.fail.on2005]), 2005],
         [all([conds.fail.on2005, conds.fail.on1995]), 2005],
       ])('should reach on latest reach, even failed ones', (cond, year) => {
-        expect(cond({})).toSatisfy(reachedAt(year))
+        expect(cond({}).reached).toSatisfy(eq.date(year))
       })
     })
   })
