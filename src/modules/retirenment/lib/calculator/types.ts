284caf027e6490e460261f4da51e16677589ef07
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-07 20:55:56 +0200

    feat: added reacher type definitions

--- src/modules/retirenment/lib/calculator/types.ts
+++ src/modules/retirenment/lib/calculator/types.ts
@@ -1,32 +1,15 @@
 /**
+ * Inputs and incoming types.
+ * --------------------------
+ */
+
+/**
  * Classification used for retirement calc purposes.
  */
 export enum Gender {
   MALE,
   FEMALE,
 }
-
-/**
- * Minimum condition context shape.
- */
-export type ConditionContextBase = { reached: Date }
-
-/**
- * A condition check result, with varying context.
- */
-export type ConditionResult<ConditionContext = ConditionContextBase> = [
-  boolean,
-  ConditionContext
-]
-
-/**
- * An adapted logic predicate function the returns the result
- * with possible result context.
- */
-export type Condition<
-  Input = object,
-  ConditionContext = ConditionContextBase
-> = (input: Input) => ConditionResult<ConditionContext>
 
 /**
  * Kinds of services.
@@ -62,3 +45,51 @@ export type Contribution = {
   salary: number
   service: Service
 }
+
+/**
+ * Reachers are atomic reach date calculators.
+ * -------------------------------------------
+ */
+
+export type Reached = Date
+
+/**
+ * Reacher execution result.
+ */
+export type ReacherResult<Context = {}> = [Reached] | [Reached, Context]
+
+/**
+ * Reacher functions.
+ *
+ * @return [0] The reaching date.
+ * @return [1] The reaching context.
+ */
+export type Reacher<Input = object, Context = {}> = (
+  input: Input
+) => ReacherResult<Context>
+
+/**
+ * Minimum condition context shape.
+ */
+export type ConditionContextBase = { reached: Date | null }
+
+/**
+ * ConditionContext factory.
+ */
+export type ConditionContext<T extends {}> = T & ConditionContextBase
+
+/**
+ * A condition check result, with varying context.
+ */
+export type ConditionResult<ConditionContext = ConditionContextBase> = [
+  boolean,
+  ConditionContext
+]
+
+/**
+ * An adapted logic predicate function the returns the result
+ * with possible result context.
+ */
+export type Condition<Input = object, T = {}> = (
+  input: Input
+) => ConditionResult<ConditionContext<T>>
