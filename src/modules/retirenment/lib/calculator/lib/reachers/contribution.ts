be6d4e714e591829605ddadb98c6efce33ddca76
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-08 00:31:00 +0200

    test: added total split config testing

--- src/modules/retirenment/lib/calculator/lib/reachers/contribution.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/contribution.ts
@@ -182,7 +182,7 @@ const utils = {
     const { start, end } = contribution
 
     // no split necessary:
-    if (start > date || (end && end < date)) return [contribution]
+    if (start >= date || (end && end <= date)) return [contribution]
 
     const left = { ...contribution, start, end: min([end || TODAY, date]) }
     const right = { ...contribution, start: max([start, date]), end }
