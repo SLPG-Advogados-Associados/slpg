fbabd089a6a6d1027aa5cbe65398840c33f06865
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-10 19:42:40 +0200

    refactor: injected field helper as prop on form

--- src/modules/form/index.tsx
+++ src/modules/form/index.tsx
@@ -1,4 +1,4 @@
 export * from './components/Input'
 export * from './components/fields'
-export * from './components/Field'
+export * from './components/FieldWrapper'
 export * from './hooks/useForm'
