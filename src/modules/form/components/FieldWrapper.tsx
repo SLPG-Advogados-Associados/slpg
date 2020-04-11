217b6a371b810fd0285863134cdabfa59d553e6f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-12 00:26:19 +0200

    feat: allow non-label wrapped FieldWrapper

--- src/modules/form/components/FieldWrapper.tsx
+++ src/modules/form/components/FieldWrapper.tsx
@@ -2,15 +2,29 @@ import React from 'react'
 import classnames from 'classnames'
 import { ErrorMessage } from './ErrorMessage'
 
-const FieldWrapper: React.FC<{
+const FieldWrapper = <
+  C extends keyof JSX.IntrinsicElements | React.ComponentType<any>
+>({
+  touched,
+  error,
+  className,
+  children,
+  wrapper,
+}: {
   touched?: boolean
   error?: React.ReactNode
   className?: string
-}> = ({ touched, error, className, children }) => (
-  <label className={classnames('mb-4 block', className)}>
-    {children}
-    <ErrorMessage touched={touched} error={error} />
-  </label>
-)
+  children: React.ReactNode
+  wrapper?: C
+}) => {
+  const Wrapper = wrapper ? wrapper : 'label'
+
+  return (
+    <Wrapper className={classnames('mb-4 block', className)}>
+      {children}
+      <ErrorMessage touched={touched} error={error} />
+    </Wrapper>
+  )
+}
 
 export { FieldWrapper }
