bacb6a8f6cbae8045a9b026dccbd225e50b8976d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-12 17:34:26 +0200

    feat: added basic date from string Yup schema

--- src/modules/form/lib/normalize.ts
+++ src/modules/form/lib/normalize.ts
@@ -1,15 +1,25 @@
-import { TransformFunction, DateSchema } from 'yup'
-import { parse as parseDate } from 'date-fns'
+import * as Yup from 'yup'
+import { parse as parseDate, isValid } from 'date-fns'
 
 const format = {}
 
 const parse = {
-  stringToDate: (format = 'dd/MM/yyyy'): TransformFunction<DateSchema> =>
-    function(value: Date | string, originalValue: Date | string) {
+  stringToDate: (
+    format = 'dd/MM/yyyy'
+  ): Yup.TransformFunction<Yup.DateSchema> =>
+    function(value: Date | string, originalValue: string) {
       if (this.isType(value)) return value
-      if (typeof originalValue !== 'string') return null
-      return parseDate(originalValue, format, new Date())
+      if (!originalValue) return null
+      const parsed = parseDate(originalValue, format, new Date())
+      return isValid(parsed) ? parsed : originalValue
     },
 }
 
-export { format, parse }
+const schemas = {
+  date: (format?: string) =>
+    Yup.date()
+      .nullable()
+      .transform(parse.stringToDate(format)),
+}
+
+export { format, parse, schemas }
