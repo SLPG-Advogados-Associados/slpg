07601bf9f6421bc52b198c61db85791a9ba45bd5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-12 16:52:55 +0200

    refactor: trust error from react-hook-form

--- src/modules/form/components/FieldWrapper.tsx
+++ src/modules/form/components/FieldWrapper.tsx
@@ -5,24 +5,25 @@ import { ErrorMessage } from './ErrorMessage'
 const FieldWrapper = <
   C extends keyof JSX.IntrinsicElements | React.ComponentType<any>
 >({
-  touched,
   error,
   className,
   children,
   wrapper,
+  label,
 }: {
-  touched?: boolean
   error?: React.ReactNode
   className?: string
   children: React.ReactNode
   wrapper?: C
+  label?: string
 }) => {
   const Wrapper = wrapper ? wrapper : 'label'
 
   return (
     <Wrapper className={classnames('mb-4 block', className)}>
+      {label ? <strong className="mb-1 block">{label}</strong> : null}
       {children}
-      <ErrorMessage touched={touched} error={error} />
+      <ErrorMessage error={error} />
     </Wrapper>
   )
 }
