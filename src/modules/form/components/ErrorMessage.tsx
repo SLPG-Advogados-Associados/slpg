c335717a9a1109bd569d7500821959b2bc1a545b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-12 16:52:55 +0200

    refactor: trust error from react-hook-form

--- src/modules/form/components/ErrorMessage.tsx
+++ src/modules/form/components/ErrorMessage.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const ErrorMessage: React.FC<{
-  touched?: boolean
   error?: React.ReactNode
-}> = ({ touched, error }) =>
-  touched && error ? (
+}> = ({ error }) =>
+  error ? (
     <span className="text-meta text-danger block p-2">{error}</span>
   ) : null
 
