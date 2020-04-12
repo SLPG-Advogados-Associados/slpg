0ac26089f72af3da800232939ccbc07df27e9a3d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-12 19:36:10 +0200

    cleanup: removed unused test

--- src/modules/retirenment/lib/calculator/calculator.test.ts
+++
@@ -1,8 +0,0 @@
-import { Calculator } from './'
-
-describe('retirement/calculator', () => {
-  it('should be possible to instantiate new calculator', () => {
-    const calculator = new Calculator()
-    expect(calculator).toBeInstanceOf(Calculator)
-  })
-})
