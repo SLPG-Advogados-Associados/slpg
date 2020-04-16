6c70e5f2ad8a5d7a229df980ecfd63f1f156f1ca
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-16 19:18:24 +0200

    refactor: updated 1988 rule to match new possibilities based format

--- src/modules/retirenment/lib/calculator/rules/1988-cf.test.ts
+++ src/modules/retirenment/lib/calculator/rules/1988-cf.test.ts
@@ -1,15 +1,15 @@
 /* cspell: disable */
-import { c, d } from '../lib/test-utils'
+import { c, d, u } from '../lib/test-utils'
 import { NEVER } from '../lib/const'
 import { Gender, Contribution, Post } from '../types'
-import { conditions } from './1988-cf'
+import { rule } from './1988-cf'
 
-const { TEACHER } = Post
+const { TEACHER: T } = Post
 const { MALE: M, FEMALE: F } = Gender
 
 describe('retirement/calculator/rules/cf-1988', () => {
   describe('conditions', () => {
-    const [condA, condB, condC, condD] = conditions
+    const [integral, proportional] = rule.possibilities
 
     const i = (
       gender: Gender,
@@ -21,94 +21,102 @@ describe('retirement/calculator/rules/cf-1988', () => {
       contributions,
     })
 
-    /**
-     * a) aos trinta e cinco anos de serviço, se homem, e aos trinta, se mulher,
-     * com proventos integrais;
-     */
-    describe('a)', () => {
-      it.each([
-        // male
-        [i(M, [c('63')]), true, d('1997-12-23')], //   ✅ +35 contribution years by 1998
-        [i(M, [c('64')]), false, d('1998-12-23')], //  ❌ -35 contribution years by 1998
-        // female
-        [i(F, [c('67')]), true, d('1996-12-24')], //   ✅ +30 contribution years by 1998
-        [i(F, [c('69')]), false, d('1998-12-25')], //  ❌ -30 contribution years by 1998
-      ])('should check qualification', (input, qualified, by) => {
-        const [reached, context] = condA(input)
-        expect(reached).toEqual(qualified)
-        expect(context.reached).toEqual(by)
-        expect(context.integrality).toBe(true)
+    describe('integral', () => {
+      const [general, teacher] = integral.conditions
+
+      /**
+       * a) aos trinta e cinco anos de serviço, se homem, e aos trinta, se mulher,
+       * com proventos integrais;
+       */
+      describe('a)', () => {
+        it.each([
+          // male
+          [i(M, [c('63')]), d('1997-12-23')],
+          [i(M, [c('64')]), d('1998-12-23')],
+          [i(M, [c('1980')]), d('2014-12-23')],
+          // female
+          [i(F, [c('67')]), d('1996-12-24')],
+          [i(F, [c('69')]), d('1998-12-25')],
+          [i(F, [c('1980')]), d('2009-12-24')],
+        ])('should check qualification', (input, expected) => {
+          const [reached] = general.execute(input)
+          expect(reached).toEqual(expected)
+        })
+      })
+
+      /**
+       * b) aos trinta anos de efetivo exercício em funções de magistério, se
+       * professor, e vinte e cinco, se professora, com proventos integrais;
+       */
+      describe('b)', () => {
+        it.each([
+          // male teacher
+          [i(M, [c('67', [u, T])]), d('1996-12-24')], //
+          [i(M, [c('69', [u, T])]), d('1998-12-25')],
+
+          // female teacher
+          [i(F, [c('72', [u, T])]), d('1996-12-25')],
+          [i(F, [c('74', [u, T])]), d('1998-12-26')],
+
+          // non-teacher
+          [i(M, [c('60')]), NEVER],
+          [i(F, [c('60')]), NEVER],
+
+          // mixed post types
+
+          // male teacher
+          [i(M, [c('60^70'), c('70', [u, T])]), d('1999-12-25')],
+          [i(M, [c('60^67'), c('67', [u, T])]), d('1996-12-24')],
+          [i(M, [c('60^67'), c('69', [u, T])]), d('1998-12-25')],
+
+          // female teacher
+          [i(F, [c('60^75'), c('75', [u, T])]), d('1999-12-26')],
+          [i(F, [c('60^72'), c('72', [u, T])]), d('1996-12-25')],
+          [i(F, [c('60^72'), c('74', [u, T])]), d('1998-12-26')],
+        ])('should check qualification', (input, expected) => {
+          const [reached] = teacher.execute(input)
+          expect(reached).toEqual(expected)
+        })
       })
     })
 
-    /**
-     * b) aos trinta anos de efetivo exercício em funções de magistério, se
-     * professor, e vinte e cinco, se professora, com proventos integrais;
-     */
-    describe('b)', () => {
-      it.each([
-        // male teacher
-        [i(M, [c('67', [null, TEACHER])]), true, d('1996-12-24')], //   ✅ +30 contribution years by 1998
-        [i(M, [c('69', [null, TEACHER])]), false, d('1998-12-25')], //  ❌ -30 contribution years by 1998
-        // female teacher
-        [i(F, [c('72', [null, TEACHER])]), true, d('1996-12-25')], //   ✅ +25 contribution years by 1998
-        [i(F, [c('74', [null, TEACHER])]), false, d('1998-12-26')], //  ❌ -25 contribution years by 1998
-        // non-teacher
-        [i(M, [c('60')]), false, NEVER], //  ✅ not-teacher
-        [i(F, [c('60')]), false, NEVER], //  ❌ not-teacher
-        // mixed post types
-        [i(M, [c('60^70'), c('70', [null, TEACHER])]), false, d('1999-12-25')], //  ❌ -30 contribution years by 1998
-        [i(M, [c('60^67'), c('67', [null, TEACHER])]), true, d('1996-12-24')], //   ✅ +30 contribution years by 1998
-        [i(M, [c('60^67'), c('69', [null, TEACHER])]), false, d('1998-12-25')], //  ❌ -30 contribution years by 1998
+    describe('proportional', () => {
+      const [contribution, age] = proportional.conditions
 
-        [i(F, [c('60^75'), c('75', [null, TEACHER])]), false, d('1999-12-26')], //  ❌ -25 contribution years by 1998
-        [i(F, [c('60^72'), c('72', [null, TEACHER])]), true, d('1996-12-25')], //   ✅ +25 contribution years by 1998
-        [i(F, [c('60^72'), c('74', [null, TEACHER])]), false, d('1998-12-26')], //  ❌ -25 contribution years by 1998
-      ])('should check qualification', (input, qualified, by) => {
-        const [reached, context] = condB(input)
-        expect(reached).toEqual(qualified)
-        expect(context.reached).toEqual(by)
-        expect(context.integrality).toBe(true)
+      /**
+       * c) aos trinta anos de serviço, se homem, e aos vinte e cinco, se mulher,
+       * com proventos proporcionais a esse tempo;
+       */
+      describe('c)', () => {
+        it.each([
+          // male
+          [i(M, [c('67')]), d('1996-12-24')],
+          [i(M, [c('69')]), d('1998-12-25')],
+          // female
+          [i(F, [c('72')]), d('1996-12-25')],
+          [i(F, [c('74')]), d('1998-12-26')],
+        ])('should check qualification', (input, expected) => {
+          const [reached] = contribution.execute(input)
+          expect(reached).toEqual(expected)
+        })
       })
-    })
 
-    /**
-     * c) aos trinta anos de serviço, se homem, e aos vinte e cinco, se mulher,
-     * com proventos proporcionais a esse tempo;
-     */
-    describe('c)', () => {
-      it.each([
-        // male
-        [i(M, [c('67')]), true, d('1996-12-24')], //   ✅ +30 contribution years by 1998
-        [i(M, [c('69')]), false, d('1998-12-25')], //  ❌ -30 contribution years by 1998
-        // female
-        [i(F, [c('72')]), true, d('1996-12-25')], //   ✅ +25 contribution years by 1998
-        [i(F, [c('74')]), false, d('1998-12-26')], //  ❌ -25 contribution years by 1998
-      ])('should check qualification', (input, qualified, by) => {
-        const [reached, context] = condC(input)
-        expect(reached).toEqual(qualified)
-        expect(context.reached).toEqual(by)
-        expect(context.integrality).toBe(false)
-      })
-    })
-
-    /**
-     * d) aos sessenta e cinco anos de idade, se homem, e aos sessenta, se mulher,
-     * com proventos proporcionais ao tempo de serviço.
-     */
-    describe('d)', () => {
-      it.each([
-        // male
-        [i(M, [], d('1932')), true, d('1997')], //   ✅ +65 years old by 1998
-        [i(M, [], d('1934')), false, d('1999')], //  ❌ -65 years old by 1998
-        // female
-        [i(F, [], d('1937')), true, d('1997')], //   ✅ +60 years old by 1998
-        [i(F, [], d('1939')), false, d('1999')], //  ❌ -60 years old by 1998
-      ])('should check qualification', (input, qualified, by) => {
-        const [reached, context] = condD(input)
-        expect(reached).toEqual(qualified)
-        expect(context.reached).toEqual(by)
-        expect(context.integrality).toBe(false)
+      /**
+       * d) aos sessenta e cinco anos de idade, se homem, e aos sessenta, se mulher,
+       * com proventos proporcionais ao tempo de serviço.
+       */
+      describe('d)', () => {
+        it.each([
+          // male
+          [i(M, [], d('1932')), d('1997')],
+          [i(M, [], d('1934')), d('1999')],
+          // female
+          [i(F, [], d('1937')), d('1997')],
+          [i(F, [], d('1939')), d('1999')],
+        ])('should check qualification', (input, expecte) => {
+          const [reached] = age.execute(input)
+          expect(reached).toEqual(expecte)
+        })
       })
     })
   })
