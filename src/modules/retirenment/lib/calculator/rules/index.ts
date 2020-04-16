352f18d15ec980894ed466e49850de58682e3fa5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-16 20:39:47 +0200

    feat: added initial calculator

---
+++ src/modules/retirenment/lib/calculator/rules/index.ts
@@ -0,0 +1,6 @@
+import { rule as Rule1988 } from './1988-cf'
+import { rule as Rule1998Transition } from './1998-ec-20-transition'
+
+const rules = [Rule1988, Rule1998Transition]
+
+export { rules }
