30c4d65d365af008d3fd9a5fc55aacf44d579a29
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-02 16:23:10 +0200

    feat: added improved t.theme to handle arrays

--- src/design/lib/styled.ts
+++ src/design/lib/styled.ts
@@ -9,7 +9,19 @@ const predicate = <P extends {}>(fn, apply: Interpolation<P>, props: P) =>
 const variants = <P extends {}>(map: { [key: string]: Interpolation<any> }) =>
   Object.keys(map).map(prop => styledTools.ifProp(prop, map[prop]))
 
+/**
+ * Extended styled-tools#theme to parse array outcomes into CSS valid lists.
+ */
+const theme = <T = undefined>(path: string, defaultValue?: T) => <Props, Theme>(
+  props: Props & { theme: Theme }
+) => [].concat(styledTools.theme(path, defaultValue)(props)).join(', ')
+
 // "t" stands for "theme", but it's just a shortcut aggregator anyway.
-const t = { ...styledTools, if: curry(predicate), variants: curry(variants) }
+const t = {
+  ...styledTools,
+  if: curry(predicate),
+  variants: curry(variants),
+  theme,
+}
 
 export { styled, css, t, classnames }
