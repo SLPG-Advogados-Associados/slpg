f32523f04500bf8a59cedadb825c577647a7819b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-08-02 18:52:25 +0200

    feat: simplified reachers entrypoint api

--- src/modules/retirenment/lib/calculator/lib/reachers/index.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/index.ts
@@ -1,28 +1,3 @@
-/* cspell: disable */
-import { add } from '../date'
-import { DurationInput, between } from '../duration'
-import { Reacher, CalculatorInput } from '../../types'
-import * as contribution from './contribution'
-
-/**
- * Age reacher factory.
- *
- * @param duration Age of reaching.
- * @param input Person input.
- */
-const age = (
-  duration: DurationInput | ((input: CalculatorInput) => DurationInput),
-  config: { due?: Date } = {}
-): Reacher => input => [
-  add(
-    input.birthDate,
-    typeof duration === 'function' ? duration(input) : duration,
-    true
-  ),
-  {
-    reachable: false,
-    byDue: config.due ? between(input.birthDate, config.due) : null,
-  },
-]
-
-export { age, contribution }
+export * from './age'
+export * from './sex'
+export * as contribution from './contribution'
