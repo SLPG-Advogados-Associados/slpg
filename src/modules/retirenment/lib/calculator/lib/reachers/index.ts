2c07eb7d3b852831017ca830d59dca00a5e66d0a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-08-09 23:53:34 +0200

    refactor: avoid babel rewire issues

--- src/modules/retirenment/lib/calculator/lib/reachers/index.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/index.ts
@@ -1,4 +1,4 @@
-export * from './age'
-export * from './sex'
-export * from './after'
+export { age } from './age'
+export { sex } from './sex'
+export { after } from './after'
 export * as contribution from './contribution'
