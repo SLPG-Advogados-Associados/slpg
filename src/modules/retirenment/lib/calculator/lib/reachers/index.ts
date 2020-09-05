bbeeb960386fe901ed987b32c3f990d43a162464
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-09-05 15:07:42 +0200

    typing: export Processor type from reachers

--- src/modules/retirenment/lib/calculator/lib/reachers/index.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/index.ts
@@ -2,3 +2,5 @@ export { age } from './age'
 export { sex } from './sex'
 export { after } from './after'
 export * as contribution from './contribution'
+
+export type { Processor } from './contribution'
