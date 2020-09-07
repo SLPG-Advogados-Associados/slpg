ebb9ed2693cf3f70c99eb1856a82e64b74e4e378
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-09-07 22:11:12 +0200

    feat: added FUTURE constant

--- src/modules/retirenment/lib/calculator/lib/const.ts
+++ src/modules/retirenment/lib/calculator/lib/const.ts
@@ -1,7 +1,9 @@
 import { parse } from 'duration-fns'
+import { add } from './date'
 
 const TODAY = new Date()
 const NEVER = new Date(NaN)
+const FUTURE = add(TODAY, { years: 100 })
 const NO_DURATION = parse('PT0S')
 
-export { TODAY, NEVER, NO_DURATION }
+export { TODAY, FUTURE, NEVER, NO_DURATION }
