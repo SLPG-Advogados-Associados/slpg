250eda405958e9cd6597944bfa8ee9c81455ec69
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-09-08 14:33:22 +0200

    test: added FUTURE mock

--- jest.setup.js
+++ jest.setup.js
@@ -3,4 +3,5 @@
 jest.mock('./src/modules/retirenment/lib/calculator/lib/const', () => ({
   ...jest.requireActual('./src/modules/retirenment/lib/calculator/lib/const'),
   TODAY: new Date('2020'),
+  FUTURE: new Date('2020'), // this shouldn't be done this way, probably...
 }))
