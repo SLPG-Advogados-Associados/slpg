27a4695093e13136b1759500635cfd029ff9d241
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-21 23:16:21 +0200

    feat: updated career tests

--- src/modules/retirenment/lib/calculator/lib/reachers/contribution/career.test.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/contribution/career.test.ts
@@ -1,42 +1,45 @@
-/* cspell: disable */
-import { Contribution } from '../../../types'
-import { d, c, u } from '../../test-utils'
+import { Contribution, ServiceKind } from '../../../types'
+import { d, contribution, u } from '../../test-utils'
 import { career } from './career'
 
-const o = expect.objectContaining
+const { PRIVATE: PR, PUBLIC } = ServiceKind
 
 describe('retirement/calculator/lib/requisites/contribution/career', () => {
   const i = (...contributions: Contribution[]) => ({ contributions })
 
-  const same: [undefined, undefined, number] = [u, u, 1]
+  const c = (span: string, career: number, kind?: ServiceKind) =>
+    contribution(span, [kind, u, career])
 
-  // prettier-ignore
+  const r = (from: string, to?: string) => ({
+    from: d(from),
+    to: to ? d(to) : u,
+  })
+
+  const isPublic = ({ service: { kind } }: Contribution) => kind === PUBLIC
+
   it.each([
-    // duration, due, contributions | satisfied, satisfied at, satisfiable, duration by due
-    [10, d('2000'), i(c('80')), true, d('1989-12-29'), true, 20],
-    [20, d('2000'), i(c('80')), true, d('1999-12-27'), true, 20],
-    [20, d('2000'), i(c('50^75'), c('80')), true, d('1999-12-27'), true, 20],
-    [10, d('2000'), i(c('70^90')), true, d('1979-12-30'), true, 20],
-    [10, d('2000'), i(c('50^75'), c('70^90')), true, d('1979-12-30'), true, 20],
-    [20, d('2000'), i(c('50^75'), c('70^90')), true, d('1989-12-27'), true, 20],
-    [20, d('2000'), i(c('81')), false, u, false, 19],
-    [20, d('2000'), i(c('50^75'), c('81')), false, u, false, 19],
-    [20, d('2000'), i(c('80^90')), false, u, false, 10],
-    [20, d('2000'), i(c('50^75'), c('80^90')), false, u, false, 10],
-    // will satisfy in time
-    [10, d('2030'), i(c('2020')), false, u, true, 10],
+    // single constraint contribution
+    [10, u, i(c('80^90', 1)), [r('90', '90')]],
+    [10, u, i(c('80^95', 1)), [r('90', '95')]],
+    [10, u, i(c('80^85', 1)), []],
 
-    // career combination
-    [10, d('2000'), i(c('90^95', same), c('95^2000', same)), true, d('1999-12-30'), true, 10],
-  ])(
-    'should correctly calculate results',
-    (years, due, input, satisfied, satisfiedAt, satisfiable, yearsByDue) => {
-      expect(career({ expected: { years }, due })(input)).toMatchObject({
-        satisfied,
-        satisfiedAt,
-        satisfiable,
-        context: { durationByDue: o({ years: yearsByDue }) },
-      })
-    }
-  )
+    // single no-end contribution
+    [10, u, i(c('80^', 1)), [r('90', u)]],
+
+    // multiple contribution, same career
+    [10, u, i(c('75^80', 1), c('80^90', 1)), [r('85', '90')]],
+    [10, u, i(c('70^80', 1), c('85^95', 1)), [r('80', '80'), r('95', '95')]],
+    [10, u, i(c('80^85', 1), c('90^95', 1)), []],
+
+    // multiple contribution, different career
+    [10, u, i(c('75^80', 1), c('80^90', 2)), [r('90', '90')]],
+    [10, u, i(c('70^80', 1), c('85^95', 2)), [r('80', '80'), r('95', '95')]],
+    [10, u, i(c('80^85', 1), c('90^95', 2)), []],
+
+    // filtered
+    [10, () => false, i(c('80^90', 1), c('90^95', 1)), []],
+    [10, isPublic, i(c('50^80', 1, PR), c('80^90', 1)), [r('90', '90')]],
+  ])('should correctly calculate results', (years, filter, input, result) => {
+    expect(career({ expected: { years }, filter })(input)).toEqual(result)
+  })
 })
--- src/modules/retirenment/lib/calculator/lib/reachers/contribution/career.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/contribution/career.ts
@@ -1,65 +1,39 @@
-import { CalculatorInput } from '../../../types'
-import { add, min } from '../../date'
-import { TODAY } from '../../const'
-import { between, DurationInput } from '../../duration'
-import { RequisiteExecutor } from '../../engine'
+import { groupWith } from 'ramda'
+import { CalculatorInput, Contribution, RequisiteResults } from '../../../types'
+import { add } from '../../date'
+import { DurationInput } from '../../duration'
 
-type Params = { due: Date; expected: DurationInput }
+type Params = {
+  expected: DurationInput
+  filter: (contribution: Contribution) => boolean
+}
+
 type Input = Pick<CalculatorInput, 'contributions'>
+
+const groupCareers = groupWith<Contribution>(
+  (a, b) => a.service.career === b.service.career && a.end && a.end >= b.start
+)
 
 /**
  * Career duration requisite factory.
  *
- * @todo consider the possibility that another career before the last
- * be the one that the retirement time was reached first.
- *
- * @todo should allow filtering contribution time for public service.
- *
  * @param expected The expected duration of the last contribution time.
  * @param due Date by which the expected duration must be achieved.
  */
-const career = ({ expected, due }: Params): RequisiteExecutor<Input> => (
-  input
-) => {
-  // find contributions that belong to same career as last post.
-  const contributions = [...input.contributions]
-    .reverse()
-    .filter(
-      ({ service: { career } }, _, arr) => career === arr[0].service.career
-    )
-    .reverse()
-
-  const first = contributions[0]
-  const last = contributions[contributions.length - 1]
-
-  if (!first || !last) {
-    return {
-      satisfied: false,
-      satisfiedAt: undefined,
-      satisfiableAt: undefined,
-      satisfiable: false,
-    }
-  }
-
-  const { start } = first
-  const { end } = last
-
-  const satisfiedAt = add(start, expected)
-  const satisfied = satisfiedAt <= min([due, end || TODAY])
-
-  // is satisfiable if it's still counting (no end) and will satisfy before due.
-  const satisfiable = satisfied || (satisfiedAt <= due && !end)
-  const satisfiableAt = satisfiable ? satisfiedAt : undefined
-
-  return {
-    satisfied,
-    satisfiedAt: satisfied ? satisfiedAt : undefined,
-    satisfiable,
-    satisfiableAt,
-    context: {
-      durationByDue: between(start, end ? min([due, end]) : due, true),
-    },
-  }
-}
+const career = ({ expected, filter = () => true }: Params) => (
+  input: Input
+): RequisiteResults =>
+  groupCareers(input.contributions.filter(filter))
+    // get first and last from career, they are all that matters here
+    // last might be same as first, and it's ok
+    .map((career) => [career[0], career[career.length - 1]])
+    // get start of first, and end of last
+    .map(([{ start }, { end }]) => ({
+      from: add(start, expected, true),
+      to: end,
+    }))
+    // it's only satisfiable if "from" ("start" of first) date happens before
+    // "to" ("end" of last) of this contribution.
+    .filter(({ from, to }) => !to || from <= to)
 
 export { career }
