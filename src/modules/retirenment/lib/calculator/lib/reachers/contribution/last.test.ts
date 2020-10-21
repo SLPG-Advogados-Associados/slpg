6100f1db28ebb6bd1b36ec53e8d18bded518427e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-21 23:17:20 +0200

    feat: updated last reacher

--- src/modules/retirenment/lib/calculator/lib/reachers/contribution/last.test.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/contribution/last.test.ts
@@ -1,37 +1,39 @@
 /* cspell: disable */
-import { Contribution } from '../../../types'
+import { Contribution, ServiceKind } from '../../../types'
 import { d, c, u } from '../../test-utils'
 import { last } from './last'
 
-const o = expect.objectContaining
+const { PRIVATE, PUBLIC } = ServiceKind
 
 describe('retirement/calculator/lib/requisites/contribution/last', () => {
   const i = (...contributions: Contribution[]) => ({ contributions })
 
-  // prettier-ignore
+  const r = (from: string, to?: string) => ({
+    from: d(from),
+    to: to ? d(to) : u,
+  })
+
+  const isPublic = ({ service: { kind } }: Contribution) => kind === PUBLIC
+
   it.each([
-    // duration, due, contributions | satisfied, satisfied at, satisfiable, duration by due
-    [10, d('2000'), i(c('80')), true, d('1989-12-29'), true, 20],
-    [20, d('2000'), i(c('80')), true, d('1999-12-27'), true, 20],
-    [20, d('2000'), i(c('50^75'), c('80')), true, d('1999-12-27'), true, 20],
-    [10, d('2000'), i(c('70^90')), true, d('1979-12-30'), true, 20],
-    [10, d('2000'), i(c('50^75'), c('70^90')), true, d('1979-12-30'), true, 20],
-    [20, d('2000'), i(c('50^75'), c('70^90')), true, d('1989-12-27'), true, 20],
-    [20, d('2000'), i(c('81')), false, u, false, 19],
-    [20, d('2000'), i(c('50^75'), c('81')), false, u, false, 19],
-    [20, d('2000'), i(c('80^90')), false, u, false, 10],
-    [20, d('2000'), i(c('50^75'), c('80^90')), false, u, false, 10],
-    // will satisfy in time
-    [10, d('2030'), i(c('2020')), false, u, true, 10],
-  ])(
-    'should correctly calculate results',
-    (years, due, input, satisfied, satisfiedAt, satisfiable, yearsByDue) => {
-      expect(last({ expected: { years }, due })(input)).toMatchObject({
-        satisfied,
-        satisfiedAt,
-        satisfiable,
-        context: { durationByDue: o({ years: yearsByDue }) },
-      })
-    }
-  )
+    // single constraint contribution
+    [10, u, i(c('80^90')), [r('90', '90')]],
+    [10, u, i(c('80^95')), [r('90', '95')]],
+    [10, u, i(c('80^85')), []],
+
+    // single no-end contribution
+    [10, u, i(c('80^')), [r('90', u)]],
+
+    // multiple contribution
+    [10, u, i(c('80^90'), c('95^2005')), [r('90', '90'), r('2005', '2005')]],
+    [10, u, i(c('80^95'), c('95^2010')), [r('90', '95'), r('2005', '2010')]],
+    [10, u, i(c('80^85'), c('95^2010')), [r('2005', '2010')]],
+    [10, u, i(c('80^85'), c('95^2000')), []],
+
+    // filtered
+    [10, () => false, i(c('80^90')), []],
+    [10, isPublic, i(c('50^80', [PRIVATE]), c('80^90')), [r('90', '90')]],
+  ])('should correctly calculate results', (years, filter, input, result) => {
+    expect(last({ expected: { years }, filter })(input)).toEqual(result)
+  })
 })
--- src/modules/retirenment/lib/calculator/lib/reachers/contribution/last.ts
+++ src/modules/retirenment/lib/calculator/lib/reachers/contribution/last.ts
@@ -1,55 +1,27 @@
-import { CalculatorInput } from '../../../types'
-import { add, min } from '../../date'
-import { TODAY } from '../../const'
-import { between, DurationInput } from '../../duration'
-import { RequisiteExecutor } from '../../engine'
+import { CalculatorInput, Contribution, RequisiteResults } from '../../../types'
+import { add } from '../../date'
+import { DurationInput } from '../../duration'
 
-type Params = { due: Date; expected: DurationInput }
+type Params = {
+  expected: DurationInput
+  filter: (contribution: Contribution) => boolean
+}
+
 type Input = Pick<CalculatorInput, 'contributions'>
 
 /**
  * Last contribution duration requisite factory.
  *
- * @todo consider the possibility that another post before the last
- * be the one that the retirement time was reached first.
- *
- * @todo should allow filtering contribution time.
- *
  * @param expected The expected duration of the last contribution time.
  * @param due Date by which the expected duration must be achieved.
  */
-const last = ({ expected, due }: Params): RequisiteExecutor<Input> => (
-  input
-) => {
-  const last = input.contributions[input.contributions.length - 1]
-
-  if (!last) {
-    return {
-      satisfied: false,
-      satisfiedAt: undefined,
-      satisfiableAt: undefined,
-      satisfiable: false,
-    }
-  }
-
-  const { start, end } = last
-
-  const satisfiedAt = add(start, expected)
-  const satisfied = satisfiedAt <= min([due, end || TODAY])
-
-  // is satisfiable if it's still counting (no end) and will satisfy before due.
-  const satisfiable = satisfied || (satisfiedAt <= due && !end)
-  const satisfiableAt = satisfiable ? satisfiedAt : undefined
-
-  return {
-    satisfied,
-    satisfiedAt: satisfied ? satisfiedAt : undefined,
-    satisfiable,
-    satisfiableAt,
-    context: {
-      durationByDue: between(start, end ? min([due, end]) : due, true),
-    },
-  }
-}
+const last = ({ expected, filter = () => true }: Params) => (
+  input: Input
+): RequisiteResults =>
+  input.contributions
+    .filter(filter)
+    .map(({ start, end }) => ({ from: add(start, expected, true), to: end }))
+    // it's only satisfiable if "from" date happens before "to" ("end") of this contribution.
+    .filter(({ from, to }) => !to || from <= to)
 
 export { last }
