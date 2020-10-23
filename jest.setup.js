8ccdcb252e6a327bb6a2e8418f8233c8dc5f57c9
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-23 17:16:40 +0200

    test: removed unnecessary jest setup

--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-jest.mock('./src/modules/retirenment/lib/calculator/lib/const', () => ({
-  ...jest.requireActual('./src/modules/retirenment/lib/calculator/lib/const'),
-  TODAY: new Date('2020'),
-  FUTURE: new Date('2020'), // this shouldn't be done this way, probably...
-}))
