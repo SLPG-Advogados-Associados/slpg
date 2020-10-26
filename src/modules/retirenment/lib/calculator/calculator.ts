607669f25db007a71efc7a829a81521aca9db7ba
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-26 13:47:45 +0100

    fix: fixed calculator root

--- src/modules/retirenment/lib/calculator/calculator.ts
+++ src/modules/retirenment/lib/calculator/calculator.ts
@@ -1,13 +1,17 @@
 import { CalculatorInput } from './types'
 import { rules } from './rules'
 
+/**
+ * Execute input against rules.
+ */
 const calculate = (input: CalculatorInput) =>
   rules.map(
-    rule =>
+    (rule) =>
       [
         rule,
         rule.possibilities.map(
-          possibility => [possibility, possibility.execute(input)] as const
+          (possibility) =>
+            [possibility, possibility.requisites.execute(input)] as const
         ),
       ] as const
   )
