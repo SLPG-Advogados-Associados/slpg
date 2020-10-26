f770efde7d04642de97e3179e05bbd6dc197679b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-26 13:48:19 +0100

    feat: simplified useCalculatorForm

--- src/modules/retirenment/lib/calculator/hooks/useCalculatorForm.ts
+++ src/modules/retirenment/lib/calculator/hooks/useCalculatorForm.ts
@@ -1,5 +1,6 @@
-import * as Yup from 'yup'
-import { useForm, schemas } from '~modules/form'
+import { useForm } from 'react-hook-form'
+import * as S from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Sex, Contribution } from '../types'
 
 type FormData = {
@@ -8,50 +9,32 @@ type FormData = {
   contributions: Contribution[]
 }
 
-const date = schemas
-  .date()
+const date = S.date()
+  .nullable()
+  .transform((curr, orig) => (orig === '' ? null : curr))
   .required('Campo obrigatório')
   .typeError('"${originalValue}" não é uma data válida')
 
-const validationSchema = Yup.object().shape({
-  sex: Yup.string().required('Campo obrigatório'),
+const schema = S.object().shape({
+  sex: S.string().required('Campo obrigatório'),
   birthDate: date,
-  contributions: Yup.array().of(
-    Yup.object().shape({
+  contributions: S.array().of(
+    S.object().shape({
       start: date,
       end: date.notRequired(),
-      service: Yup.object().shape({
-        title: Yup.string(),
-        kind: Yup.string().required('Campo obrigatório'),
-        post: Yup.string().required('Campo obrigatório'),
+      service: S.object().shape({
+        title: S.string(),
+        kind: S.string().required('Campo obrigatório'),
+        post: S.string().required('Campo obrigatório'),
+        career: S.number().required('Campo obrigatório'),
       }),
     })
   ),
 })
 
-const useCalculatorForm = () => {
-  const form = useForm<FormData>({ validationSchema })
-
-  const fields = {
-    sex: form.useField('sex'),
-    birthDate: form.useField('birthDate'),
-    contributions: form.useFieldArray('contributions', (path) => ({
-      start: form.useField(`${path}.start`, true),
-      end: form.useField(`${path}.end`, true),
-      salary: form.useField(`${path}.salary`, true),
-      service: {
-        title: form.useField(`${path}.service.title`, true),
-        kind: form.useField(`${path}.service.kind`, true),
-        post: form.useField(`${path}.service.post`, true),
-      },
-    })),
-  }
-
-  if (fields.contributions.items.length === 0) {
-    fields.contributions.append({})
-  }
-
-  return { ...form, fields }
-}
+const useCalculatorForm = () =>
+  useForm<FormData>({
+    resolver: yupResolver(schema),
+  })
 
 export { useCalculatorForm }
