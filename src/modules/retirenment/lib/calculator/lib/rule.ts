3adc11bff7156783a8dbf1efb68ae7176df7c336
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-27 23:45:14 +0100

    feat: added initial Rule class

---
+++ src/modules/retirenment/lib/calculator/lib/rule.ts
@@ -0,0 +1,60 @@
+import { all } from './engine/result'
+import { Possibility, CalculatorInput } from '../types'
+
+export interface RuleConfig {
+  title: string
+  description: string
+  due?: Date
+  promulgation: Date
+  possibilities: Possibility[]
+}
+
+/**
+ * A law ruling and it's related retirenment possibilities.
+ */
+class Rule {
+  /**
+   * A human title of the rule.
+   */
+  public title: string
+
+  /**
+   * A description of rule the effect.
+   */
+  public description: string
+
+  /**
+   * Date when this rule became valid.
+   */
+  public promulgation: Date
+
+  /**
+   * Date when the rule became invalid.
+   */
+  public due?: Date
+
+  /**
+   * Retirenment possibilities described by this rule.
+   */
+  public possibilities: Possibility[]
+
+  constructor(config: RuleConfig) {
+    this.title = config.title
+    this.description = config.description
+    this.promulgation = config.promulgation
+    this.due = config.due
+    this.possibilities = config.possibilities
+  }
+
+  /**
+   * Execute a possibility requisites respecting promulgation/due dates.
+   */
+  public executePossibility(index: number, input: CalculatorInput) {
+    return all([
+      [{ from: this.promulgation, to: this.due }],
+      this.possibilities[index].requisites.execute(input),
+    ])
+  }
+}
+
+export { Rule }
