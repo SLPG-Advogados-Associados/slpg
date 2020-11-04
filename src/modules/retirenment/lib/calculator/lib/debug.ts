61275e09633e11db8d69781ed9fa65f3d3e18a7b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-11-04 17:39:27 +0100

    typing: fixed types on debug helpers

--- src/modules/retirenment/lib/calculator/lib/debug.ts
+++ src/modules/retirenment/lib/calculator/lib/debug.ts
@@ -2,7 +2,7 @@ import { format } from 'date-fns'
 import { DurationInput } from 'duration-fns'
 import { asTree, TreeObject } from 'treeify'
 
-import { RequisiteResult, RequisiteChain } from './engine/index'
+import { Period, RequisiteChain } from './engine'
 
 const str = {
   /**
@@ -23,21 +23,22 @@ const str = {
     const executor = 'any' in chain || 'all' in chain ? null : chain.executor
     const prefix = group ? `[${group}] ` : ''
     const name = chain.title ?? chain.description ?? executor?.toString() ?? ''
-    const result = str.results(chain.lastResult || []) || 'N'
 
-    return `${(prefix + name).trim()}: ${result}`
+    return `${(prefix + name).trim()}`
+    // const result = str.results(chain.lastResult || []) || 'N'
+
+    // return `${(prefix + name).trim()}: ${result}`
   },
 
   /**
    * String representation of one result.
    */
-  result: ({ from, to }: RequisiteResult) =>
-    `${str.date(from)}^${str.date(to)}`,
+  result: ({ from, to }: Period) => `${str.date(from)}^${str.date(to)}`,
 
   /**
    * String representation of multiple results.
    */
-  results: (results: RequisiteResult[]) => results.map(str.result).join(', '),
+  results: (results: Period[]) => results.map(str.result).join(', '),
 
   /**
    * String tree representation of an object.
