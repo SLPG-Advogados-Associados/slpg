6015d22985c6c5bb0de53aff576f5cbfa9134009
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-11-04 12:38:10 +0100

    feat: reuse 1998 possibilities on 2003 rule

--- src/modules/retirenment/lib/calculator/rules/2003-ec-41/2003-ec-41.rule.ts
+++ src/modules/retirenment/lib/calculator/rules/2003-ec-41/2003-ec-41.rule.ts
@@ -1,8 +1,10 @@
 import { Rule } from '../../lib/engine/rule'
 import { dates } from '../dates'
 
-import { possibility as art40integral } from './possibilities/art-40--permanent--integral'
-import { possibility as art40proportional } from './possibilities/art-40--permanent--proportional'
+// inherited
+import { possibility as art40integral } from '../1998-ec-20/possibilities/art-40--permanent--integral'
+import { possibility as art40proportional } from '../1998-ec-20/possibilities/art-40--permanent--proportional'
+
 import { possibility as art2 } from './possibilities/art-2--transition'
 import { possibility as art6 } from './possibilities/art-6--transition'
 
@@ -14,7 +16,7 @@ const rule = new Rule({
   due,
   title: 'EC nº 41',
   description: 'Regra permanente como descrita na EC nº 41, de 2003',
-  possibilities: [art40integral, art40proportional, art2, art6],
+  possibilities: [art40integral.clone(), art40proportional.clone(), art2, art6],
 })
 
 export { rule }
